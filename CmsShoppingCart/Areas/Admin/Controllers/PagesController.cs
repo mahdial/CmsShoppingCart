9b9eba707c92c000540ef75ce617e13272a9b858
mahdi alizadeh <Mahdi.al@gmail.com>
2018-10-27 03:23:01 +0330

    Crate tblPages Create pages model (PageDTO and PageVM) set DbSet in Db Create Pages controller and view

---
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,26 @@
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            List<PageVM> pageList;
+
+            using (Db db = new Db())
+            {
+                pageList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+                return View(pageList);
+        }
+    }
+}
