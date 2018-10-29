4c57429e2ca40b1983e20ba25ee77fd8c4a9ddf0
mahdi alizadeh <Mahdi.al@gmail.com>
2018-10-29 03:30:48 +0330

    Add CKEDITOR and fileman to project and config ckeditor to use fileman as a filemanager to upload doc and img. add ckeditor with fileman to edit page and edit sidebar

--- CmsShoppingCart/Models/ViewModels/Pages/SidebarVM.cs
+++ CmsShoppingCart/Models/ViewModels/Pages/SidebarVM.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace CmsShoppingCart.Models.ViewModels.Pages
 {
@@ -24,6 +25,7 @@ namespace CmsShoppingCart.Models.ViewModels.Pages
 
         [Required]
         [StringLength(int.MaxValue, MinimumLength = 5)]
+        [AllowHtml]
         public string Body { get; set; }
     }
 }
